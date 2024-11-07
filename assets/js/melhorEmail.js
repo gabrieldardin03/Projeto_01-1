bc4bfcfee83d07cc134ff3b790045302b23f3aaa
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-06 21:31:10 -0300

    Update melhorEmail.js

--- assets/js/melhorEmail.js
+++ assets/js/melhorEmail.js
@@ -1,3 +1,23 @@
-$(function(){
+const button = document.querySelector('submit');
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    //Inputs do formulário
+    const email = document.querySelector('input[type=email]').ariaValueMax
     
-})
+    //Endpoint do SheetDB
+    fetch('https://sheetdb.io/api/v1/b45btxqg69q27', {
+        method: 'post',
+        headers:{
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            email
+        })
+    })
+alert('E-mail enviado com sucesso!');
+window.location.href = '';
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
