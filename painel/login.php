4dc64e7499124452c3d5da00f9ef514e1d9aff57
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-25 22:13:45 -0300

    update

--- painel/login.php
+++ painel/login.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL;?>css/style.css">
     <title>Painel de controle</title>
 </head>
@@ -25,7 +29,7 @@
                     header('Location: '.INCLUDE_PATH_PAINEL);
                     die();
                 }else{
-                    echo '<div class="erro-box"> Usuário ou senha incorretos!</div>';
+                    echo '<div class="erro-box"><i class="fa-solid fa-x"></i> &nbsp Usuário ou senha incorretos!</div>';
                 }
             }
         ?>
--- painel/main.php
+++ painel/main.php
@@ -1,1 +1,35 @@
-<p>You're on Main page.</p>
+<!DOCTYPE html>
+<html lang="en, pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL;?>css/style.css">
+    <title>Painel de controle</title>
+</head>
+
+<body>
+
+    <aside>
+    </aside>
+
+    <header>
+
+        <div class="center">
+            <div class="logout">
+                <a href="<?php echo INCLUDE_PATH_PAINEL;?>?logout">
+                    <i class="fa fa-sign-out" aria-hidden="true"></i>
+                </a>
+            </div>
+            <!--logout-->
+            <div class="clear"></div>
+        </div>
+        <!--center-->
+    </header>
+</body>
+
+</html>
