6678fadfbaebd637782dbee671cd3aef39a53a15
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-12-11 20:15:04 -0300

    update

--- painel/main.php
+++ painel/main.php
@@ -74,23 +74,7 @@ if (isset($_GET['logout'])) {
     </header>
 
     <div class="content">
-        <div class="box-content left w100">
-            <h2><i class="fa-solid fa-house"></i>Painel de Controle - <?php echo $nomeEmpresa ?></h2>
-            <div class="box-metricas">
-                <div class="box-metrica-single">
-                    <h2>Usuários Online</h2>
-                    <p>10</p>
-                </div>
-                <div class="box-metrica-single">
-                    <h2>Visitas Hoje</h2>
-                    <p>20</p>
-                </div>
-                <div class="box-metrica-single">
-                    <h2>Visitas Totais</h2>
-                    <p>30</p>
-                </div>
-            </div>
-        </div>
+
     </div>
 
     <!-- Jquery -->
