23f371e08be327ff9a2e101a6bda74c69e715b01
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2025-01-28 22:06:43 -0300

    Update listar-depoimentos.php

--- painel/pages/listar-depoimentos.php
+++ painel/pages/listar-depoimentos.php
@@ -5,19 +5,24 @@ $depoimentos = Painel::getAll('tb_admin.depoimentos');
 <div class="box-content">
     <h2><i class="fas fa-database"></i> Depoimentos cadastrados</h2>
 
-    <table>
-        <tr>
-            <td>Data</td>
-            <td>Nome</td>
-            <td>Editar</td>
-            <td>Excluir</td>
-        </tr>
-        <tr>
-            <td>28/01/2025</td>
-            <td>Lucas</td>
-            <td><a href="" class="edit"><i class="fas fa-edit"></i></a></td>
-            <td><a href="" class="delete"><i class="fas fa-trash"></i></a></td>
-        </tr>
-    </table>
+    <div class="wraper-table">
+        <table>
+            <tr>
+                <td>Data</td>
+                <td>Nome</td>
+                <td>Editar</td>
+                <td>Excluir</td>
+            </tr>
+            <?php foreach ($depoimentos as $key => $value){ ?>
+                <tr>
+                    <td><?php echo $value['data']; ?></td>
+                    <td><?php echo $value['nome']; ?></td>
+                    <td><a href="" class="edit"><i class="fas fa-edit"></i></a></td>
+                    <td><a href="" class="delete"><i class="fas fa-trash"></i></a></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+    <!--wraper table-->
 </div>
 <!--box content-->
